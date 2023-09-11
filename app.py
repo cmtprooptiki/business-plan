097be3dc4648166acdf81b62fc932f803f81ce35
A-PALIOS <paliosadonis6@gmail.com>
2023-09-11 13:13:07 +0300

    message

--- app.py
+++ app.py
@@ -418,6 +418,43 @@ def e_button9(id,kpdf):
         #     st.success("Record Updated Successfully")
         #     st.experimental_rerun()
     if(option=="export"):
+
+        mycursor.execute("select * from forms where koispe_id="+str(id)+"")
+        result = mycursor.fetchall()
+        # for row in result:
+        #     st.write(row)
+            # Extract values from the "return_id" column and store them in a list
+        return_ids = [row[0] for row in result]
+        return_creation_date=[row[2] for row in result]
+        return_year=[row[3] for row in result]
+        return_identifierform=["Year:"+row[3]+" Creation Date:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" ID FORM:"+str(row[0]) for row in result]
+        # st.write(return_identifierform)
+         #getAllformsId
+        # st.write(str(return_ids))
+        # st.write(str(return_creation_date))
+        # Convert the list of datetime objects to a list of strings
+        date_str_list = [return_creation_date.strftime("%Y-%m-%d %H:%M:%S") for return_creation_date in return_creation_date]
+
+        # st.write(date_str_list)
+        # st.write(str(return_year))
+
+        #option=st.selectbox("Select an Form",date_str_list)
+
+        #st.write("You choose",str(option))
+
+        selected_id = st.selectbox("Select a Form", options=return_identifierform, index=0)
+        selected_id_index = return_identifierform.index(selected_id)
+        selected_id_value = return_ids[selected_id_index]
+
+        # Display the selected date and its corresponding ID
+        st.write(f"Selected Date: {selected_id}")
+        st.write(f"Corresponding ID: {selected_id_value}")
+
+
+
+
+
+
         env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
         template = env.get_template("template.html")
 

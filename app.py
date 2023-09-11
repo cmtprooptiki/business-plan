eca1f13595f6d46f6f7b7bbf027b3ac31675039d
A-PALIOS <paliosadonis6@gmail.com>
2023-09-11 14:59:14 +0300

    message

--- app.py
+++ app.py
@@ -486,6 +486,14 @@ def e_button9(id,kpdf):
                         file_name="diploma.pdf",
                         mime="application/octet-stream",
                     )
+                year=row[3]
+                q1_text=row[4]
+                q1_ans_radio=row[5]
+                q2_text=row[6]
+                q2_1_ans_radio=row[7]
+                q2_2_ans_radio=row[8]
+                q3_text=row[9]
+                q3_ans_radio=row[10]
                 result_val=((int(q1_ans_radio)+int(q2_1_ans_radio)+int(q2_2_ans_radio)+int(q3_ans_radio))/4)*10
                 st.write(result_val)
                 fig=donut_pct_Chart(result_val,'#618abb', 'rgb(240,240,240)',['% Ποσοστό Ετοιμότητας', ' '])

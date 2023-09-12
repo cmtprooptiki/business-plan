4382d082e901ad1a23fa2e69c89adc766c89fcc1
A-PALIOS <paliosadonis6@gmail.com>
2023-09-12 10:57:07 +0300

    message

--- app.py
+++ app.py
@@ -377,7 +377,7 @@ def e_button9(id,kpdf):
                 # mycursor.execute("delete from forms where id="+str(row[0])+"and koispe_id="+str(row[1])+" and creation_date="+str(row[2])+"")
                 if(st.button("yes")):
                     st.write("Done")
-                    mycursor.execute("delete from forms where id="+str(row[0])+"and koispe_id="+str(row[1])+" and creation_date="+str(row[2])+"")
+                    mycursor.execute("delete from forms where id="+str(row[0])+"and koispe_id="+str(row[1])+"")
                 elif(st.button("No")):
                     pass
     if(option=="export"):

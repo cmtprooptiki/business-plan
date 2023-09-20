87a984a6b84bb5262f565e98e446dc504dc4e12c
A-PALIOS <paliosadonis6@gmail.com>
2023-09-20 16:17:53 +0300

    message

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def form1(id,kpdf):
     st.subheader("Δημιουργία Νέου Business Plan")
         
     # Encapsulate the form using st.form
-    with st.form(key="create_form",clear_on_submit=True,required=True):
+    with st.form(key="create_form",clear_on_submit=True):
         
 
         # ###QUESTION 1
@@ -277,11 +277,11 @@ def form1(id,kpdf):
         # q=""
         # age=""
         # Submit button inside the form
-        #submit_button = st.form_submit_button("Submit")
-        if title:
-            submit_button = st.form_submit_button("Submit",disabled=False)
-        else:
-            submit_button = st.form_submit_button("Submit",disabled=True)
+        submit_button = st.form_submit_button("Submit")
+        # if title:
+        #     submit_button = st.form_submit_button("Submit",disabled=False)
+        # else:
+        #     submit_button = st.form_submit_button("Submit",disabled=True)
     # Check if the submit button is clicked
     if submit_button:
         # Call the create_record function to insert the data into the database

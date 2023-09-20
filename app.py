d511dda4c596859399fdc6cba44bf3888df66309
A-PALIOS <paliosadonis6@gmail.com>
2023-09-20 10:42:42 +0300

    message

--- app.py
+++ app.py
@@ -1746,6 +1746,81 @@ def e_button10(id,kpdf):
 
             st.write("Συνεπώς, για να είναι βιώσιμη η επιχειρηματική ιδέα σας, κρίνεται απαραίτητο, το ελάχιστο των ετήσιων εσόδων να είναι: "+str(SUM_leit))
             st.warning("Για κάθε επόμενο έτος λειτουργίας της επιχειρηματικής ιδέας σας, θα πρέπει να λάβετε υπόψιν τυχόν αύξηση του λειτουργικού κόστους (π.χ αυξήσεις μισθών, ανατιμήσεις αγαθών, κλπ.) και τις αποσβέσεις.")
+    if option=="Delete":
+        mycursor.execute("select * from forms2 where koispe_id="+str(id)+"")
+        result = mycursor.fetchall()
+
+        # for row in result:
+        #     st.write(row)
+        # Extract values from the "return_id" column and store them in a list
+        return_ids = [row[0] for row in result]
+        return_creation_date=[row[2] for row in result]
+        return_year=[row[3] for row in result]
+        return_identifierform=["Year:"+row[3]+" Creation Date:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" ID FORM:"+str(row[0]) for row in result]
+        # st.write(return_identifierform)
+         #getAllformsId
+        # st.write(str(return_ids))
+        # st.write(str(return_creation_date))
+        # Convert the list of datetime objects to a list of strings
+        date_str_list = [return_creation_date.strftime("%Y-%m-%d %H:%M:%S") for return_creation_date in return_creation_date]
+
+        # st.write(date_str_list)
+        # st.write(str(return_year))
+
+        #option=st.selectbox("Select an Form",date_str_list)
+
+        #st.write("You choose",str(option))
+
+        selected_id = st.selectbox("Select a Form", options=return_identifierform, index=0)
+        selected_id_index = return_identifierform.index(selected_id)
+        selected_id_value = return_ids[selected_id_index]
+
+        # Display the selected date and its corresponding ID
+        st.write(f"Selected Date: {selected_id}")
+        st.write(f"Corresponding ID: {selected_id_value}")
+
+
+        if selected_id:
+            mycursor.execute("select * from forms where koispe_id="+str(id)+" and id="+str(selected_id_value)+"")
+            result = mycursor.fetchall()
+            for row in result:
+                st.write(row)
+
+            st.write(row[1],row[2],row[3])
+
+            if st.button("Remove"):
+                try:
+                    sql = "DELETE FROM forms WHERE id=%s AND koispe_id=%s"
+                    val = (selected_id_value, id)
+                    mycursor.execute(sql, val)
+
+                    # Commit the transaction
+                    conn.commit()
+
+                    st.success("Record Deleted Successfully")
+                    # st.experimental_rerun()
+
+                except Exception as e:
+                    st.error(f"Error deleting record: {str(e)}")
+    
+            # if st.button("remove"):
+            #     st.write(str(selected_id_value))
+            #     st.write(str(id))
+
+            #     # mycursor.execute("delete from forms where id="+str(selected_id_value)+" and koispe_id="+str(id)+" ")
+            #     sql="DELETE FROM forms WHERE id=%s and koispe_id=%s"
+            #     val=(selected_id_value,id)
+            #     mycursor.execute(sql,val)
+            #     st.success("Record Deleted Succesfully")
+                # st.error("are you sure?")
+                # # mycursor.execute("delete from forms where id="+str(row[0])+"and koispe_id="+str(row[1])+" and creation_date="+str(row[2])+"")
+                # if(st.button("yes")):
+                #     st.write("Done")
+                #     mycursor.execute("delete from forms where id="+str(selected_id_value)+" and koispe_id="+str(id)+" ")
+                #     st.success("Record Deleted Succesfully")
+                # elif(st.button("No")):
+                #     pass
+    
     if option =="export":
         st.write("hello")
         # mycursor.execute("select * from forms where koispe_id="+str(id)+"")

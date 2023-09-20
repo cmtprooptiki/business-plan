2b32d2a3bb8c6c39db4935985604d90842b36f51
A-PALIOS <paliosadonis6@gmail.com>
2023-09-20 15:53:12 +0300

    message

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ def form1(id,kpdf):
         # q=""
         # age=""
         # Submit button inside the form
-        submit_button = st.form_submit_button("Submit")
+        submit_button = st.form_submit_button("Submit",disabled=not title)
     # Check if the submit button is clicked
     if submit_button:
         # Call the create_record function to insert the data into the database

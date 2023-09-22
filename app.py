e02242f9541ca005c584dd08db016cbdeaa634a9
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-09-22 12:28:29 +0300

    message

--- app.py
+++ app.py
@@ -799,7 +799,7 @@ def e_button9(id,kpdf):
             return_creation_date=[row[2] for row in result]
             return_year=[row[3] for row in result]
             #return_identifierform=["Τιτλες:"+row[3]+" Creation Date:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" ID FORM:"+str(row[0]) for row in result]
-            return_identifierform=["Τίτλος: "+row[3]+" ID FORM:"+str(row[0]) for row in result]
+            return_identifierform=["Τίτλος: "+row[3]+" Κωδικός Φόρμας:"+str(row[0]) for row in result]
 
             # st.write(return_identifierform)
             #getAllformsId
@@ -1134,7 +1134,7 @@ def e_button9(id,kpdf):
             return_ids = [row[0] for row in result]
             return_creation_date=[row[2] for row in result]
             return_year=[row[3] for row in result]
-            return_identifierform=["Year:"+row[3]+" Creation Date:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" ID FORM:"+str(row[0]) for row in result]
+            return_identifierform=["Τίτλος:"+row[3]+" Ημ/νια Δημιουργίας:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" Κωδικός Φόρμας:"+str(row[0]) for row in result]
             # st.write(return_identifierform)
             #getAllformsId
             # st.write(str(return_ids))
@@ -1212,7 +1212,7 @@ def e_button9(id,kpdf):
             return_ids = [row[0] for row in result]
             return_creation_date=[row[2] for row in result]
             return_year=[row[3] for row in result]
-            return_identifierform=["Title:"+row[3]+" Creation Date:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" ID FORM:"+str(row[0]) for row in result]
+            return_identifierform=["Τίτλος:"+row[3]+" Ημ/νια Δημιουργίας:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" Κωδικός Φόρμας:"+str(row[0]) for row in result]
             # st.write(return_identifierform)
             #getAllformsId
             # st.write(str(return_ids))
@@ -1431,7 +1431,7 @@ def e_button10(id,kpdf):
             return_creation_date=[row[2] for row in result]
             return_year=[row[3] for row in result]
             #return_identifierform=["Τίτλος:"+row[3]+" Creation Date:"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" ID FORM:"+str(row[0]) for row in result]
-            return_identifierform=["Τίτλος: "+row[3]+" ID FORM:"+str(row[0]) for row in result]
+            return_identifierform=["Τίτλος: "+row[3]+" Ημ/νια Δημιουργίας::"+row[2].strftime("%Y-%m-%d %H:%M:%S")+" Κωδικός φόρμας:"+str(row[0]) for row in result]
             # st.write(return_identifierform)
             #getAllformsId
             # st.write(str(return_ids))

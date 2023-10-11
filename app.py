59a9331d882f1e008db240d7a11688ee296826cb
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-10-11 14:53:21 +0300

    message

--- app.py
+++ app.py
@@ -1482,7 +1482,7 @@ def e_button10(id,kpdf):
 
                     q6_text = st.text_area("Γράψε ελεύθερο κείμενο", key="q6text",value=row[4], height=300)
                     st.markdown("<h4>Κόστος(€)</h4>",unsafe_allow_html=True)
-                    q6_1_ans_num=st.number_input('Συμπληρώστε νούμερο:',key="q6_1_ans_num", step=0.00,value=row[5])
+                    q6_1_ans_num=st.number_input('Συμπληρώστε νούμερο:',key="q6_1_ans_num", step=1,value=row[5])
                     q6_1_calc=q6_1_ans_num*0.04
 
 
